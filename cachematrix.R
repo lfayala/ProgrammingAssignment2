72c8cebbed842b2cde7854cb9adebba0aea0835d
lfayala <layala@ufm.edu>
2016-07-15 13:53:39 -0600

    Programming assignment 2
    by layala

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function AYALA
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  M <- NULL
+  set <- function(y) {
+    x <<- y
+    M <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) M <<- solve
+  getinverse <- function() M
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
